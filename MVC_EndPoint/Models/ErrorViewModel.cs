0d2f8869847590063a7e30d80f5d45467b1002ad
AREF BAHMANI <arefbahmani021@gmail.com>
2025-02-25 04:38:24 +0330

    fixed

--- MVC_EndPoint/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_EndPoint.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
